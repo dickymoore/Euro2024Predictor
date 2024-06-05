e168739350f5f757bc0d106d5ce82bca06930c9b
dickymoorebjss <128724856+dickymoorebjss@users.noreply.github.com>
2024-06-05 13:51:43 +0100

    Cache sorted

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
+import pandas as pd
 import pickle
 from datetime import datetime
 
-from scripts.data_ingestion import load_data
 from scripts.data_transform import transform_data
 from scripts.data_store import store_data
 from scripts.config import load_config
@@ -12,51 +12,60 @@ def get_file_modification_time(file_path):
 
 def read_cache_timestamp(cache_timestamp_path):
     if os.path.exists(cache_timestamp_path):
-        with open(cache_timestamp_path, 'rb') as file:
-            return pickle.load(file)
+        if os.path.getsize(cache_timestamp_path) > 0:
+            try:
+                with open(cache_timestamp_path, 'r') as file:
+                    cache_timestamp_string = file.read().strip()
+                    cache_timestamp = datetime.strptime(cache_timestamp_string, "%Y-%m-%d %H:%M:%S")
+                    print(f"Cache time: {cache_timestamp}")
+                    return cache_timestamp
+            except EOFError:
+                print(f"Error: {cache_timestamp_path} is empty or corrupted.")
+                return None
+            except Exception as e:
+                print(f"Error reading {cache_timestamp_path}: {e}")
+                return None
+        else:
+            print(f"Error: {cache_timestamp_path} is empty.")
+            return None
     return None
 
-def write_cache_timestamp(cache_timestamp_path, timestamp):
-    with open(cache_timestamp_path, 'wb') as file:
-        pickle.dump(timestamp, file)
-
 def main():
-    print("Loading configuration...")
     config = load_config()
     teams = config['teams']
     look_back_months = config['look_back_months']
-    print(f"""Configuration is: 
+    print(f"""
+Configuration is: 
 teams={teams} 
-look_back_months={look_back_months}""")
+look_back_months={look_back_months}
+""")
 
-    # csv_path = 'data/raw/data.csv'
-    # cache_path = 'data/cache/data.pkl'
-    # cache_timestamp_path = 'data/cache/data_timestamp.pkl'
+    csv_path = 'data/raw/results.csv'
+    cache_path = 'data/cache/data.pkl'
+    cache_timestamp_path = 'data/cache/data_timestamp.pkl'
 
-    # print("Checking data freshness...")
-    # raw_data_mod_time = get_file_modification_time(csv_path)
-    # cache_data_mod_time = read_cache_timestamp(cache_timestamp_path)
+    raw_data_mod_time = get_file_modification_time(csv_path)
+    cache_data_mod_time = read_cache_timestamp(cache_timestamp_path)
+    print("Checking data freshness...")
+    print(f"Raw results data last updated: {raw_data_mod_time}")
+    print(f"Cached results data updated: {cache_data_mod_time}")
+    
+    if cache_data_mod_time is None or raw_data_mod_time > cache_data_mod_time:
+        print(f"Loading data from {csv_path}")
+        raw_data = pd.read_csv(csv_path)
+        num_raw_rows = raw_data.shape[0]
+        print(f'{num_raw_rows} lines of raw_data have been loaded from {csv_path}.')
+        transformed_data = transform_data(raw_data, teams, look_back_months)
+        num_transformed_rows = transformed_data.shape[0]
+        print(f'{num_transformed_rows} lines of transformed data.')
+        assert num_transformed_rows < num_raw_rows, f"Error: Transformed data ({num_transformed_rows} lines) is not less than raw data ({num_raw_rows} lines)."
+        store_data(transformed_data, cache_path,cache_timestamp_path)
+    else:
+        print("Loading data from cache...")
+        data = pd.read_pickle(cache_path)
+        num_cached_rows = data.shape[0]
+        print(f'{num_cached_rows} lines of raw_data have been loaded from cache.')
 
-    # if cache_data_mod_time and raw_data_mod_time <= cache_data_mod_time:
-    #     print("Loading data from cache...")
-    #     data = load_data(cache_path)
-    #     print("Data loaded from cache.")
-    # else:
-    #     print("Loading data from raw source...")
-    #     data = load_data(csv_path, cache_path=None)
-    #     print("Data loaded from raw source.")
-
-    #     print("Transforming data...")
-    #     data = transform_data(data, teams, look_back_months)
-    #     print("Data transformed successfully.")
-
-    #     print("Storing data to cache...")
-    #     store_data(data, cache_path)
-    #     write_cache_timestamp(cache_timestamp_path, raw_data_mod_time)
-    #     print("Data stored to cache.")
-
-    # print("Outputting data preview...")
-    # print(data.head())
 
 if __name__ == "__main__":
     main()
--- scripts/data_store.py
+++ scripts/data_store.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import pickle
+from datetime import datetime
 
-def store_data(data, cache_path):
+def store_data(data, cache_path,cache_timestamp_path):
     data.to_pickle(cache_path)
+    with open(cache_timestamp_path, 'w') as file:
+        current_time = datetime.now()  # Define current_time here
+        file.write(current_time.strftime("%Y-%m-%d %H:%M:%S"))  # Store the current time in a specific string format
