cb3ac4c0699fd0c5f5ee2274dff3ac1eb709b7dc
mihalytorley <mihaly.torley@bjss.com>
2024-06-07 16:37:44 +0100

    more magic

--- weighted_win_percentage.py
+++ weighted_win_percentage.py
@@ -39,6 +39,7 @@ def getWinPercentage(teams, data):
             new_row = {"country" : team, "win_percentage" : 0, "no_games" : numberOfGames}
         winPercentageAllCountries.loc[len(winPercentageAllCountries)] = new_row
     # print(winPercentageAllCountries)
+    winPercentageAllCountries.to_csv("winPercentage.csv", sep=',', index=False, encoding='utf-8')
     return winPercentageAllCountries
     
 
@@ -59,29 +60,66 @@ def weightedScoring(away_score, home_score, away_country, home_country, away_cou
     country_away = away_country
     country_home = home_country
     def scoring(winning_score, loosing_score, winning_country_win_percentage, loosing_country_win_percentage):
-        number = (float(winning_score) * (100 - float(winning_country_win_percentage))) - (float(loosing_score) * (100 - float(loosing_country_win_percentage)))
-        return number
+        if winning_score == loosing_score:
+            winning_number = (float(winning_score) * (float(loosing_country_win_percentage)/100))
+            loosing_number = (float(loosing_score) * (float(winning_country_win_percentage)/100))
+        else:
+            winning_number = (float(winning_score) * (float(loosing_country_win_percentage)/100)) + (float(loosing_score) * (float(winning_country_win_percentage)/100))
+            loosing_number = (float(winning_score) * (float(loosing_country_win_percentage)/100)) - (float(loosing_score) * (float(winning_country_win_percentage)/100))
+        return [winning_number, loosing_number]
+    winning_country_weighted_score = scoring(away_score, home_score, away_country_win_percentage, home_country_win_percentage)[0]
+    loosing_country_weighted_score = scoring(away_score, home_score, away_country_win_percentage, home_country_win_percentage)[1]
     if away_score > home_score:
         winner_country = away_country
-        away_country_weighted_score = scoring(away_score, home_score, away_country_win_percentage, home_country_win_percentage)
-        home_country_weighted_score = scoring(home_score, away_score, home_country_win_percentage, away_country_win_percentage)
-    elif home_score > away_score:
-        winner_country = home_country
-        away_country_weighted_score = scoring(away_score, home_score, away_country_win_percentage, home_country_win_percentage)
-        home_country_weighted_score = scoring(home_score, away_score, home_country_win_percentage, away_country_win_percentage)
+        away_country_weighted_score = winning_country_weighted_score
+        home_country_weighted_score = loosing_country_weighted_score
     else:
-        away_country_weighted_score = ((1 + float(away_score)) * (100 - float(away_country_win_percentage))) - ((1 + float(home_score)) * (100 - float(home_country_win_percentage)))
-        home_country_weighted_score = ((1 + float(home_score)) * (100 - float(home_country_win_percentage))) - ((1 + float(away_score)) * (100 - float(away_country_win_percentage)))
-    return {"winner_country": winner_country, "country_away": country_away, "country_home": country_home, "away_country_weighted_score": away_country_weighted_score, "home_country_weighted_score": home_country_weighted_score}
+        winner_country = home_country
+        home_country_weighted_score = winning_country_weighted_score
+        away_country_weighted_score = loosing_country_weighted_score
+    return {"winner_country": winner_country, "country_away": country_away, "country_home": country_home, "away_score": away_score, "home_score": home_score, "away_country_win_percentage": away_country_win_percentage, "home_country_win_percentage": home_country_win_percentage,  "away_country_weighted_score": away_country_weighted_score, "home_country_weighted_score": home_country_weighted_score}
     
     
 
 def sortData():
-    weightedScoringTable = pd.DataFrame(columns=("winner_country", "country_away", "country_home", "away_country_weighted_score", "home_country_weighted_score"))
+    weightedScoringTable = pd.DataFrame(columns=("winner_country", "country_away", "country_home", "away_score", "home_score", "away_country_win_percentage", "home_country_win_percentage", "away_country_weighted_score", "home_country_weighted_score"))
     winPercentageTable = getWinPercentage(allTeams, dataAll)
     for row in euroCountriesGames.itertuples():
-        new_row = weightedScoring(row.away_score, row.home_score, row.away_team, row.home_team, winPercentageTable.loc[winPercentageTable["country"] == row.away_team]["win_percentage"], winPercentageTable.loc[winPercentageTable["country"] == row.home_team]["win_percentage"])
+        new_row = weightedScoring(row.away_score, row.home_score, row.away_team, row.home_team, float(winPercentageTable.loc[winPercentageTable["country"] == row.away_team, "win_percentage"]), float(winPercentageTable.loc[winPercentageTable["country"] == row.home_team, "win_percentage"]))
         #getWinPercentage([row.away_team], dataAll)[row.away_team], getWinPercentage([row.home_team], dataAll)[row.home_team]
         weightedScoringTable.loc[len(weightedScoringTable)] = new_row
-    weightedScoringTable.to_csv("filename.csv", sep=',', index=False, encoding='utf-8')
-sortData()
+    # weightedScoringTable.to_csv("filename.csv", sep=',', index=False, encoding='utf-8')
+    return weightedScoringTable
+
+def sortHomeScore(team, data):
+    totalWeightedScore = 0
+    data = data[data.country_home == team]
+    for row in data.itertuples():
+        totalWeightedScore += float(row.home_country_weighted_score)
+    return totalWeightedScore
+
+def sortAwayScore(team, data):
+    totalWeightedScore = 0
+    data = data[data.country_away == team]
+    for row in data.itertuples():
+        totalWeightedScore += float(row.away_country_weighted_score)
+    return totalWeightedScore
+
+def getWeightedScores():
+    allGamesWeightedScoringTable = sortData().dropna()
+    weightedScoresAllCountries = pd.DataFrame(columns=("country", "weighted_score", "no.games"))
+    for team in allTeams:
+        numberOfGames = allGamesWeightedScoringTable[allGamesWeightedScoringTable.country_away.isin([team]) | allGamesWeightedScoringTable.country_home.isin([team])].shape[0]
+        totalWeightedScore = 0
+        totalWeightedScore += sortHomeScore(team, allGamesWeightedScoringTable)
+        totalWeightedScore += sortAwayScore(team, allGamesWeightedScoringTable)
+        if totalWeightedScore == 0:
+            new_row = {"country": team, "weighted_score": 0, "no.games": numberOfGames}
+        else:    
+            new_row = {"country": team, "weighted_score": totalWeightedScore/numberOfGames, "no.games": numberOfGames}
+        weightedScoresAllCountries.loc[len(weightedScoresAllCountries)] = new_row
+    weightedScoresAllCountries.to_csv("weightedScoring.csv", sep=',', index=False, encoding='utf-8')
+
+getWeightedScores()
+    
+    
